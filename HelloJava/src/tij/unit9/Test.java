a4d0d58433fd256c4731664d931e65f0e7d87f4c
zhang ying <emailzhangy@163.com>
2019-04-09 18:35:50 +0800

    接口

--- HelloJava/src/tij/unit9/Test.java
+++ HelloJava/src/tij/unit9/Test.java
@@ -1,5 +1,24 @@
 package tij.unit9;
 
+interface I1 {
+    void f();
+}
+
+class I2 implements I1 {
+    @Override
+    public void f() {
+        System.out.println("f2 I2");
+    }
+}
+
+class I3 extends I2 implements I1{
+    @Override
+    public void f() {
+        System.out.println("f2 I3");
+    }
+}
+
+
 /**
  * Test class
  *
@@ -9,6 +28,8 @@ package tij.unit9;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        I2 i3 = new I3();
+        i3.f();
     }
 }
