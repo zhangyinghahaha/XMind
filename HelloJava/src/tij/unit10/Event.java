81909844f7cceb77b4a1f2b106187820099e1b7d
ying.zhang01 <emailzhangy@163.com>
2019-04-22 11:18:28 +0800

    内部类

---
+++ HelloJava/src/tij/unit10/Event.java
@@ -0,0 +1,24 @@
+package tij.unit10;
+
+/**
+ * Event class
+ *
+ * @author ying.zhang01
+ * @date 2019/4/20
+ */
+public abstract class Event {
+    private long eventTime;
+    protected final long delayTime;
+    public Event(long delayTime) {
+        this.delayTime = delayTime;
+        start();
+    }
+
+    public void start() {
+        eventTime = System.nanoTime() + delayTime;
+    }
+    public boolean ready() {
+        return System.nanoTime() >= eventTime;
+    }
+    public abstract void action();
+}
