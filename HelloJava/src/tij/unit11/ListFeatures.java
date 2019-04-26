aae349372fde8b81d5f8c6dc45feb6c59fb5b5ac
ying.zhang01 <emailzhangy@163.com>
2019-04-26 18:03:21 +0800

    容器类

---
+++ HelloJava/src/tij/unit11/ListFeatures.java
@@ -0,0 +1,16 @@
+package tij.unit11;
+
+import java.util.Random;
+
+/**
+ * ListFeatures class
+ *
+ * @author ying.zhang01
+ * @date 2019/4/26
+ */
+public class ListFeatures {
+    public static void main(String[] args) {
+//        Random rand = new Random(47);
+//        List<Pet> pets = Pets.arrayList(7);
+    }
+}
