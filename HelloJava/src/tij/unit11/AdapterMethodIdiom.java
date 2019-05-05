11fe4331adeaee72521ec42cf4b84aa7c7bac751
ying.zhang01 <emailzhangy@163.com>
2019-05-05 19:55:10 +0800

    容器类

---
+++ HelloJava/src/tij/unit11/AdapterMethodIdiom.java
@@ -0,0 +1,16 @@
+package tij.unit11;
+
+import java.util.ArrayList;
+
+class ReversibleArrayList<T> extends ArrayList<T> {
+    
+}
+
+/**
+ * AdapterMethodIdiom class
+ *
+ * @author ying.zhang01
+ * @date 2019/5/5
+ */
+public class AdapterMethodIdiom {
+}
