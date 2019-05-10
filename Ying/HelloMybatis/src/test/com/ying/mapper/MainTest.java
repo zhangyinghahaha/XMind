81d8bf0a5857119cac650c3d190d4d3a0a4a4e4c
ying.zhang01 <emailzhangy@163.com>
2019-05-10 18:00:42 +0800

    mysql

---
+++ Ying/HelloMybatis/src/test/com/ying/mapper/MainTest.java
@@ -0,0 +1,15 @@
+package com.ying.mapper;
+
+/**
+ * MainTest class
+ *
+ * @author ying.zhang01
+ * @date 2019/5/10
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        String a = null;
+        String b = a+"";
+        System.out.println(b.equals("null"));
+    }
+}
