0a0e6cf00c82d4680dab1f01d855769351334938
ying.zhang01 <emailzhangy@163.com>
2019-05-21 18:06:56 +0800

    异常类

--- HelloJava/src/tij/unit12/DynamicFields.java
+++ HelloJava/src/tij/unit12/DynamicFields.java
@@ -65,5 +65,48 @@ public class DynamicFields {
         return makeField(id);
     }
 
-    
+    public Object getField(String id) throws NoSuchFieldException {
+        return fields[getFieldNumber(id)][1];
+    }
+
+    public Object setField(String id, Object value) throws DynamicFieldsException {
+        if(value == null) {
+            DynamicFieldsException dfe = new DynamicFieldsException();
+            dfe.initCause(new NullPointerException());
+            throw dfe;
+        }
+        int fieldNumber = hasField(id);
+        if(fieldNumber == -1) {
+            fieldNumber = makeField(id);
+        }
+        Object result = null;
+        try {
+            result = getField(id);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        fields[fieldNumber][1] = value;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+
+        try {
+            df.setField("d", "A value for d");
+            df.setField("number", 47);
+            df.setField("number2", 48);
+            System.out.println(df);
+            df.setField("d", "A new value for d2");
+            df.setField("number3", 11);
+            System.out.println("df: \n" + df);
+            System.out.println("df.getField(d): " + df.getField("d"));
+            Object field = df.setField("d", null);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace(System.out);
+        } catch (DynamicFieldsException e) {
+            e.printStackTrace(System.out);
+        }
+    }
 }
--- HelloJava/src/tij/unit12/FullConstructors.java
+++ HelloJava/src/tij/unit12/FullConstructors.java
@@ -18,7 +18,7 @@ class MyException extends Exception {
 public class FullConstructors {
     public static void f() throws MyException {
         System.out.println("Throwing MyException from f()");
-        //throw new MyException();
+        throw new MyException();
     }
     public static void g() throws MyException {
         System.out.println("Throwing MyException from g()");
