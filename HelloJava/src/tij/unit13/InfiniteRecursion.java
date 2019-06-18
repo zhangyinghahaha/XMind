7824be935d1e23ac62cc78cb66d484b47efa5090
ying.zhang01 <emailzhangy@163.com>
2019-06-18 19:56:24 +0800

    String

---
+++ HelloJava/src/tij/unit13/InfiniteRecursion.java
@@ -0,0 +1,25 @@
+package tij.unit13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * InfiniteRecursion class
+ *
+ * @author ying.zhang01
+ * @date 2019/6/18
+ */
+public class InfiniteRecursion {
+    @Override
+    public String toString() {
+        return " InfiniteRecursion address: " + super.toString() + "\n";
+    }
+
+    public static void main(String[] args) {
+        List<InfiniteRecursion> v = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            v.add(new InfiniteRecursion());
+        }
+        System.out.println(v);
+    }
+}
