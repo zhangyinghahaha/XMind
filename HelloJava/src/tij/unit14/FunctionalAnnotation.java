28a2916ba0261415d8b118c95dc0e66ee96c2967
ying.zhang01 <emailzhangy@163.com>
2019-09-30 17:59:43 +0800

    函数式编程

--- HelloJava/src/tij/unit14/FunctionalAnnotation.java
+++ HelloJava/src/tij/unit14/FunctionalAnnotation.java
@@ -1,6 +1,13 @@
 package tij.unit14;
 
+@FunctionalInterface
+interface Functional {
+    String goodbye(String arg);
+}
 
+interface FunctionalNoAnn {
+    String goodbye(String arg);
+}
 
 /**
  * FunctionalAnnotation class
@@ -9,4 +16,17 @@ package tij.unit14;
  * @date 2019/9/9
  */
 public class FunctionalAnnotation {
+    public String goodbye(String arg) {
+        return "Goodebye, " + arg;
+    }
+
+    public static void main(String[] args) {
+        FunctionalAnnotation fa = new FunctionalAnnotation();
+        Functional f = fa::goodbye;
+        FunctionalNoAnn fna = fa::goodbye;
+        Functional fl = a -> "Goodbye, " + a;
+        FunctionalNoAnn fnal = a -> "Goodbye, " + a;
+
+        System.out.println(fl.goodbye("zhang ying"));
+    }
 }
