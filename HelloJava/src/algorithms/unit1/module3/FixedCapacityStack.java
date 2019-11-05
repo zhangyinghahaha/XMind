f1b2322359b7f7591b5aaf6dd752c705cecb56da
ying.zhang01 <emailzhangy@163.com>
2019-11-05 17:58:15 +0800

    背包、栈

---
+++ HelloJava/src/algorithms/unit1/module3/FixedCapacityStack.java
@@ -0,0 +1,66 @@
+package algorithms.unit1.module3;
+
+import java.util.Iterator;
+
+/**
+ * FixedCapacityStackOfStrings class
+ *
+ * @author ying.zhang01
+ * @date 2019/11/4
+ */
+public class FixedCapacityStack<Item> implements Iterable<Item> {
+    private Item[] a;
+    private int N;
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+    public int size() {
+        return N;
+    }
+    public void push(Item item) {
+        if(N == a.length) {
+            resize(2*a.length);
+        }
+        a[N++] = item;
+    }
+    public Item pop() {
+        Item item = a[--N];
+        a[N] = null;
+        if(N > 0 && N == a.length/4) {
+            resize(a.length/2);
+        }
+        return item;
+    }
+
+    private void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        for(int i = 0; i < N; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+        @Override
+        public Item next() {
+            return a[--i];
+        }
+        @Override
+        public void remove() {}
+    }
+}
---
+++ HelloJava/src/algorithms/unit1/module3/ResizingArrayStack.java
@@ -0,0 +1,22 @@
+package algorithms.unit1.module3;
+
+/**
+ * ResizingArrayStack class
+ *
+ * @author ying.zhang01
+ * @date 2019/11/5
+ */
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+    private Item[] a = (Item[]) new Object[1];
+    private int N = 0;
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+    public int size() {
+        return N;
+    }
+    private void resize(int max) {
+
+    }
+}
