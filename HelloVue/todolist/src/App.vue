ad4a63eb3afd01a8af8552631f35c3abd850428e
zhang ying <emailzhangy@163.com>
2019-11-23 01:54:10 +0800

    vue-cli

---
+++ HelloVue/todolist/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    TodoList
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ HelloVue/todolist/src/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+        <div>
+            <input type="text" v-model="inputValue">
+            <button @click="handleSubmit">提交</button>
+        </div>
+        <ul>
+            <todo-item 
+            v-for="item of list" 
+            :content="item" 
+            :index="index"
+            :key="index"
+            @delete="handleDelete">  
+            </todo-item>
+        </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+    components: {
+        'todo-item': TodoItem
+    },
+    data: function() {
+        return {
+            inputValue: "hello",
+            list: []
+        }
+    },
+    methods: {
+        handleSubmit: function() {
+            this.list.push(this.inputValue);
+            this.inputValue="";
+        },
+        handleDelete: function(index) {
+            this.list.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
