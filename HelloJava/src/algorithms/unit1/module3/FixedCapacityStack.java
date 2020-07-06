5a59ab79a89589df0b794f923a5e2b910bbad18f
ying.zhang01 <emailzhangy@163.com>
2020-07-06 18:03:40 +0800

    stack

--- HelloJava/src/algorithms/unit1/module3/FixedCapacityStack.java
+++ HelloJava/src/algorithms/unit1/module3/FixedCapacityStack.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 public class FixedCapacityStack<Item> implements Iterable<Item> {
     private Item[] a;
     private int N;
+
     public FixedCapacityStack(int cap) {
         a = (Item[]) new Object[cap];
     }
@@ -23,27 +24,31 @@ public class FixedCapacityStack<Item> implements Iterable<Item> {
     public boolean isEmpty() {
         return N == 0;
     }
+
     public int size() {
         return N;
     }
+
     public void push(Item item) {
-        if(N == a.length) {
-            resize(2*a.length);
+        if (N == a.length) {
+            resize(2 * a.length);
         }
         a[N++] = item;
     }
+
     public Item pop() {
         Item item = a[--N];
+        // 避免对象游离
         a[N] = null;
-        if(N > 0 && N == a.length/4) {
-            resize(a.length/2);
+        if (N > 0 && N == a.length / 4) {
+            resize(a.length / 2);
         }
         return item;
     }
 
     private void resize(int max) {
         Item[] temp = (Item[]) new Object[max];
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             temp[i] = a[i];
         }
         a = temp;
@@ -56,11 +61,14 @@ public class FixedCapacityStack<Item> implements Iterable<Item> {
         public boolean hasNext() {
             return i > 0;
         }
+
         @Override
         public Item next() {
             return a[--i];
         }
+
         @Override
-        public void remove() {}
+        public void remove() {
+        }
     }
 }
