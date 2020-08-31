500ff2d77e7642542afc64baebc6c91940a643b5
ying.zhang01 <emailzhangy@163.com>
2020-08-31 20:16:50 +0800

    Queue

--- HelloJava/src/algorithms/unit1/module3/Queue.java
+++ HelloJava/src/algorithms/unit1/module3/Queue.java
@@ -1,21 +1,89 @@
 package algorithms.unit1.module3;
 
-public class Queue<Item> {
+import algorithms.util.StdIn;
+import algorithms.util.StdOut;
+
+import java.util.Iterator;
+
+public class Queue<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+
     Queue() {
     }
 
     public void enqueue(Item item) {
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
+        N++;
     }
 
+
     public Item dequeue() {
-        return null;
+        Item item = first.item;
+        first = first.next;
+
+        if (isEmpty()) {
+            last = null;
+        }
+        N--;
+        return item;
     }
 
     public boolean isEmpty() {
-        return false;
+        return first == null;
     }
 
     public int size() {
-        return 0;
+        return N;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+
+    public static void main(String[] args) {
+        Queue<String> q = new Queue<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                q.enqueue(item);
+            } else if (!q.isEmpty()) {
+                StdOut.print(q.dequeue() + " ");
+            }
+        }
     }
 }
