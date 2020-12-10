6bb155f30ecb96d80dc2c8943065e8d4c61f74b5
ying.zhang01 <emailzhangy@163.com>
2020-12-10 15:30:43 +0800

    算法分析

--- HelloJava/src/algorithms/unit1/module4/Stopwatch.java
+++ HelloJava/src/algorithms/unit1/module4/Stopwatch.java
@@ -11,4 +11,15 @@ public class Stopwatch {
         long now = System.currentTimeMillis();
         return (now - start) / 1000;
     }
+
+    public static void main(String[] args) {
+        Stopwatch timer = new Stopwatch();
+        for (int i = 0; i < 100000; i++) {
+            for (int j = 0; j < 100000; j++) {
+                for (int k = 0; k < 100000; k++) {
+                }
+            }
+        }
+        System.out.println(timer.elapsedTime());
+    }
 }
