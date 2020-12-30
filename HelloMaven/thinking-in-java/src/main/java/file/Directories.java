46d75f79118513089a793df080b7600d2ca15888
ying.zhang01 <emailzhangy@163.com>
2020-12-30 18:10:40 +0800

    files

--- HelloMaven/thinking-in-java/src/main/java/file/Directories.java
+++ HelloMaven/thinking-in-java/src/main/java/file/Directories.java
@@ -1,9 +1,11 @@
 package file;
 
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Directories {
@@ -11,8 +13,47 @@ public class Directories {
     static String sep = FileSystems.getDefault().getSeparator();
     static List<String> parts = Arrays.asList("foo", "bar", "baz", "bag");
 
-    public static void main(String[] args) {
-        System.out.println(test);
-        System.out.println(sep);
+    static Path makeVariant() {
+        Collections.rotate(parts, 1);
+        return Paths.get("test", String.join(sep, parts));
+    }
+
+    static void refreshTestDir() throws Exception {
+        if (Files.exists(test)) {
+            RmDir.rmdir(test);
+        } else {
+            Files.createDirectory(test);
+        }
+    }
+
+    static void populateTestDir() throws Exception {
+        for (int i = 0; i < parts.size(); i++) {
+            Path variant = makeVariant();
+            if (!Files.exists(variant)) {
+                Files.createDirectories(variant);
+                Files.copy(test.resolve("Hello.txt"), variant.resolve("File.txt"));
+                Files.createTempFile(variant, null, null);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        refreshTestDir();
+        Files.createFile(test.resolve("Hello.txt"));
+        Path variant = makeVariant();
+        System.out.println(variant);
+
+        try {
+            Files.createDirectory(variant);
+        } catch (Exception e) {
+            System.out.println("Nope, that doesn't work.");
+        }
+
+        populateTestDir();
+        Path tempdir = Files.createTempDirectory(test, "DIR_");
+        Files.createTempFile(tempdir, "pre", ".non");
+        Files.newDirectoryStream(test).forEach(System.out::println);
+        System.out.println("-----------");
+        Files.walk(test).forEach(System.out::println);
     }
 }
