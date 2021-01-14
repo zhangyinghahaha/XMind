f2691b6dd69aa5eb5fe482248695315e60532206
zhang ying <emailzhangy@163.com>
2021-01-14 22:52:35 +0800

    type info

--- HelloMaven/thinking-in-java/src/main/java/typeinfo/GenericClassReferences.java
+++ HelloMaven/thinking-in-java/src/main/java/typeinfo/GenericClassReferences.java
@@ -3,8 +3,8 @@ package typeinfo;
 public class GenericClassReferences {
     public static void main(String[] args) {
         Class intClass = int.class;
-        Class<Integer> genericIntClass = int.class;
-        // genericIntClass = double.class;
+        Class<?> genericIntClass = int.class;
+        genericIntClass = double.class;
         intClass = double.class;
     }
 }
