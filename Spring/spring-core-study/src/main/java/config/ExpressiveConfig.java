a8c62ab9c2fa9543634f576f77a0355a73a8ebdb
zhang ying <emailzhangy@163.com>
2021-02-03 22:49:06 +0800

    SpEL

--- Spring/spring-core-study/src/main/java/config/ExpressiveConfig.java
+++ Spring/spring-core-study/src/main/java/config/ExpressiveConfig.java
@@ -5,17 +5,19 @@ import ioc.CompactDisc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:config/app.properties")
+@PropertySource(value = "classpath:app.properties")
 public class ExpressiveConfig {
     @Autowired
     Environment env;
 
     @Bean
+    @Primary
     public CompactDisc blanDisc() {
-        return new BlankDisc(env.getProperty("disc.title"), env.getProperty("disc.artist"));
+        return new BlankDisc("#{systemProperties['disc.title']}", env.getProperty("disc.artist"));
     }
 }
