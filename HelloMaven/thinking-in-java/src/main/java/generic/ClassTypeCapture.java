c5ca95eecd76481d2bf81dee2f1fdb7907c992c2
ying.zhang01 <emailzhangy@163.com>
2021-02-26 19:12:08 +0800

    generic

---
+++ HelloMaven/thinking-in-java/src/main/java/generic/ClassTypeCapture.java
@@ -0,0 +1,23 @@
+package generic;
+
+class Building {}
+
+class House extends Building {}
+
+public class ClassTypeCapture<T> {
+    Class<T> kind;
+
+    public ClassTypeCapture(Class<T> kind) {
+        this.kind = kind;
+    }
+
+    public boolean f(Object arg) {
+        return this.kind.isInstance(arg);
+    }
+
+    public static void main(String[] args) {
+        ClassTypeCapture<Building> ctt1 = new ClassTypeCapture<>(Building.class);
+        System.out.println(ctt1.f(new Building()));
+        System.out.println(ctt1.f(new House()));
+    }
+}
---
+++ HelloMaven/thinking-in-java/src/main/java/generic/Erased.java
@@ -0,0 +1,11 @@
+package generic;
+
+public class Erased<T> {
+    private final int size = 100;
+
+    public void f(Object arg) {
+//        if (arg instanceof T) {
+//
+//        }
+    }
+}
