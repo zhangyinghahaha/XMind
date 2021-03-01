2bd42ddae0470a506abd52d0f09bf402d796a995
zhang ying <emailzhangy@163.com>
2021-03-01 23:00:00 +0800

    community project

---
+++ community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -0,0 +1,15 @@
+package com.nowcoder.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String say() {
+        return "Hello, Spring Boot!";
+    }
+}
