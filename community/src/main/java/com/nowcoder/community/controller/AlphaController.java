d826fb5d96cd830ecf0859214f0dbf6f0b7b8ef4
zhang ying <emailzhangy@163.com>
2021-03-21 22:57:35 +0800

    spring mvc

--- community/src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ community/src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,8 +3,12 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 @RequestMapping("/alpha")
@@ -21,5 +25,39 @@ public class AlphaController {
 
     public String getData() {
         return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        // 获取请求数据
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        System.out.println(request.getParameter("code"));
+
+        // 返回响应数据
+        response.setContentType("text/html;charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.write("<h1>牛客网</h1>");
+        } catch (IOException ioException) {
+            System.out.println(ioException.getMessage());
+        }
+
+    }
+
+    // Get请求
+    // /students?current=1&limit=20
+    @RequestMapping(path = "/student", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(@RequestParam(name = "current", required = false, defaultValue = "1") int current, @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "Some students";
+    }
+
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        return "Student " + id;
     }
 }
