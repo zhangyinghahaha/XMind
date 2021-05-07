01f276c6a0e5a8091ee913d9386f5a0c0e61198d
zhang ying <emailzhangy@163.com>
2021-05-07 22:47:16 +0800

    过滤敏感词

---
+++ community/src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博，可以*嫖*娼*，哈哈";
+        System.out.println(sensitiveFilter.filter(text));
+    }
+}
