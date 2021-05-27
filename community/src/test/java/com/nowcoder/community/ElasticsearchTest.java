aa95fb9cf4bf4fc0e134aeebb28f72b4c63ba544
zhang ying <emailzhangy@163.com>
2021-05-27 23:10:47 +0800

    elastic

--- community/src/test/java/com/nowcoder/community/ElasticsearchTest.java
+++ community/src/test/java/com/nowcoder/community/ElasticsearchTest.java
@@ -2,10 +2,20 @@ package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Query;
 
 @SpringBootTest
 public class ElasticsearchTest {
@@ -15,8 +25,44 @@ public class ElasticsearchTest {
     @Autowired
     private DiscussPostRepository discussPostRepository;
 
+    @Autowired
+    private ElasticsearchRestTemplate template;
+
     @Test
     public void testInert() {
-        discussPostRepository.save(discussPostMapper.selectDiscussPostById(284));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(110));
+    }
+
+    @Test
+    public void testInsert() {
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(101, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(102, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(103, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(111, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(112, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(113, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(114, 0, 100));
+    }
+
+    @Test
+    public void testSearch() {
+        Query query = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        SearchHits<DiscussPost> searchHits = template.search(query, DiscussPost.class);
+        System.out.println(searchHits.getTotalHits());
+
+        for (SearchHit<DiscussPost> searchHit : searchHits) {
+            System.out.println(searchHit.getContent().getTitle());
+            System.out.println(searchHit.getHighlightFields().get("content"));
+        }
     }
 }
