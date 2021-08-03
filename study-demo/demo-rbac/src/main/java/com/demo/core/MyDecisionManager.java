4ace8f6a87ef68469fd9c086c1f652d148746b65
ying.zhang01 <emailzhangy@163.com>
2021-08-03 18:27:34 +0800

    rbac

--- study-demo/demo-rbac/src/main/java/com/demo/core/MyDecisionManager.java
+++ study-demo/demo-rbac/src/main/java/com/demo/core/MyDecisionManager.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Objects;
 
 @Component
