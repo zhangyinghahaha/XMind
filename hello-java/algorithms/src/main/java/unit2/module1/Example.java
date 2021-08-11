eea6bc95070541ff9da4fb292799ae53d9079f79
zhang ying <emailzhangy@163.com>
2021-08-12 00:52:03 +0800

    算法分析

--- hello-java/algorithms/src/main/java/unit2/module1/Example.java
+++ hello-java/algorithms/src/main/java/unit2/module1/Example.java
@@ -14,11 +14,23 @@ public class Example {
 
     }
 
+    /**
+     * 比较两个元素大小
+     * @param v
+     * @param w
+     * @return
+     */
     private static boolean less(Comparable v, Comparable w) {
         compareCount++;
         return v.compareTo(w) < 0;
     }
 
+    /**
+     * 交换两个元素位置
+     * @param a
+     * @param i
+     * @param j
+     */
     private static void exch(Comparable[] a, int i, int j) {
         Comparable t = a[i];
         a[i] = a[j];
@@ -26,6 +38,10 @@ public class Example {
         exchangeCount++;
     }
 
+    /**
+     * 打印数组内容
+     * @param a
+     */
     private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
@@ -33,6 +49,11 @@ public class Example {
         System.out.println();
     }
 
+    /**
+     * 验证数组a是否有序
+     * @param a
+     * @return
+     */
     public static boolean isSorted(Comparable[] a) {
         for (int i = 1; i < a.length; i++) {
             if (less(a[i], a[i - 1])) {
@@ -44,10 +65,12 @@ public class Example {
 
     public static void main(String[] args) {
         String[] a = In.readStrings();
+        System.out.println("排序前:");
         show(a);
         sort(a);
-        //System.out.println(isSorted(a));
+        System.out.println("排序后:");
         show(a);
+        System.out.println("是否排序:" + isSorted(a));
         System.out.println("Compare Count: " + compareCount);
         System.out.println("Exchange Count: " + exchangeCount);
     }
