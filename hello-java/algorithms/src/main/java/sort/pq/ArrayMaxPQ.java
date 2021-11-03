65e99f0c99e493851945844c6e3ffea12e95c3b1
zhang ying <zhangyingmark@gmail.com>
2021-11-04 00:58:19 +0800

    优先队列

--- hello-java/algorithms/src/main/java/sort/pq/ArrayMaxPQ.java
+++ hello-java/algorithms/src/main/java/sort/pq/ArrayMaxPQ.java
@@ -9,7 +9,7 @@ public class ArrayMaxPQ<Item extends Comparable<Item>> implements MaxPQ<Item>{
     /**
      * 元素存储数组
      */
-    private Item[] items = (Item[]) new Object[1];
+    private Item[] items = (Item[]) new Comparable[2];
 
     // 优先队列元素 && 栈顶指针
     private int size = 0;
@@ -24,30 +24,69 @@ public class ArrayMaxPQ<Item extends Comparable<Item>> implements MaxPQ<Item>{
 
     @Override
     public Item max() {
-        return null;
+        return items[maxIndex()];
     }
 
     @Override
     public Item delMax() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+
+        int maxIndex = maxIndex();
+        Item max = items[maxIndex];
+        items[maxIndex] = items[--size];
+        items[size] = null;
+        if (size > 0 && size <= items.length/4) {
+            resize(items.length/2);
+        }
+        return max;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size <= 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     private void resize(int max) {
+        // System.out.println("Resize: " + items.length + " --> " + max);
         // 将栈元素移动到一个大小为max的新数组
-        Item[] temp = (Item[]) new Object[max];
+        Item[] temp = (Item[]) new Comparable[max];
         for (int i = 0; i < size; i++) {
             temp[i] = items[i];
         }
         items = temp;
+    }
+
+    private boolean less(Item a, Item b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private int maxIndex() {
+        int max = 0;
+        for (int i = 0; i < size; i++) {
+            if (less(items[max], items[i])) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 6, 2, 4, 3};
+
+        ArrayMaxPQ<Integer> maxPQ = new ArrayMaxPQ<>();
+        for (int i = 0; i < input.length; i++) {
+            maxPQ.insert(input[i]);
+        }
+        while (!maxPQ.isEmpty()) {
+            int max = maxPQ.delMax();
+            System.out.println(max);
+        }
     }
 }
