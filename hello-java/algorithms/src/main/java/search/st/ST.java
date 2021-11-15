aab42af4d5c05f8421ea1a19c60b80a526bcdf19
zhang ying <zhangyingmark@gmail.com>
2021-11-15 08:46:11 +0800

    查找

---
+++ hello-java/algorithms/src/main/java/search/st/ST.java
@@ -0,0 +1,53 @@
+package search.st;
+
+/**
+ * 符号表
+ *
+ * @author zhangying
+ */
+public interface ST<Key, Value> {
+    /**
+     * 将键值对存入表中，若值为空则将键从表中删除
+     * @param key
+     * @param value
+     */
+    void put(Key key, Value value);
+
+    /**
+     * 获取键对应的值，若键不存在则返回空
+     * @param key
+     * @return
+     */
+    Value get(Key key);
+
+    /**
+     * 从表中删除键
+     * @param key
+     */
+    void delete(Key key);
+
+    /**
+     * 键是否在表中
+     * @param key
+     * @return
+     */
+    boolean contains(Key key);
+
+    /**
+     * 表是否为空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 表中键值对的数量
+     * @return
+     */
+    int size();
+
+    /**
+     * 表中所有键的集合
+     * @return
+     */
+    Iterable<Key> keys();
+}
