451f2397736d0495c37d2f7007c67b61a0ee712e
zhang ying <zhangyingmark@gmail.com>
2022-01-27 08:51:26 +0800

    线性探测

--- hello-java/algorithms/src/main/java/search/hash/LinearProbingHashSt.java
+++ hello-java/algorithms/src/main/java/search/hash/LinearProbingHashSt.java
@@ -6,14 +6,33 @@ package search.hash;
  * @author zhangying
  */
 public class LinearProbingHashSt<Key, Value> {
+    private static final int DEFAULT_CAPACITY = 16;
+
+    /**
+     * 符号表中键值对的数量
+     */
     private int n;
-    private int m = 16;
+    /**
+     * 线性探测表的大小
+     */
+    private int m;
+    /**
+     * 键
+     */
     private Key[] keys;
+    /**
+     * 值
+     */
     private Value[] values;
 
     public LinearProbingHashSt() {
-        keys = (Key[]) new Object[m];
-        values = (Value[]) new Object[m];
+        this(DEFAULT_CAPACITY);
+    }
+
+    public LinearProbingHashSt(int capacity) {
+        keys = (Key[]) new Object[capacity];
+        values = (Value[]) new Object[capacity];
+        this.m = capacity;
     }
 
     private int hash(Key key) {
@@ -40,5 +59,54 @@ public class LinearProbingHashSt<Key, Value> {
             }
         }
         return null;
+    }
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    public void delete(Key key) {
+        if (!contains(key)) {
+            return;
+        }
+
+        // 查找并删除key
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % m;
+        }
+        keys[i] = null;
+        values[i] = null;
+
+        // 将簇中被删除键的右侧的所有键重新插入散列表
+        i = (i + 1) % m;
+        while (keys[i] != null) {
+            Key keyToRedo = keys[i];
+            Value valueToRedo = values[i];
+            keys[i] = null;
+            values[i] = null;
+            n--;
+            put(keyToRedo, valueToRedo);
+            i = (i + 1) % m;
+        }
+
+        // 将散列表键值对数量减一，并检查是否需要调整线性探测表大小
+        n--;
+        if (n > 0 && n <= m/8) {
+            resize(m/2);
+        }
+    }
+
+    private void resize(int capacity) {
+        LinearProbingHashSt<Key, Value> tmp = new LinearProbingHashSt<>(capacity);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) {
+                tmp.put(keys[i], values[i]);
+            }
+        }
+
+        keys = tmp.keys;
+        values = tmp.values;
+        this.m = tmp.m;
     }
 }
