37f9c52b41600fab5cdcd1af612ab4e4659c79b3
ying.zhang01 <ying.zhang01@united-imaging.com>
2022-06-18 00:28:43 +0800

    vue:webpack

---
+++ vue/webpack-learn/02_webpack_css/src/js/element.js
@@ -0,0 +1,8 @@
+import '../css/style.css';
+import '../css/title.less';
+
+const divEl = document.createElement('div');
+divEl.className = 'title';
+divEl.innerHTML = '你好啊，张颖';
+
+document.body.appendChild(divEl);
---
+++ vue/webpack-learn/02_webpack_css/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // loader: 'css-loader'
+                use: [
+                    'style-loader',
+                    'css-loader', 
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')]
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                // loader: 'css-loader'
+                use: [
+                    'style-loader',
+                    'css-loader', 
+                    'less-loader',
+                ]
+            }
+        ],
+    }
+}
