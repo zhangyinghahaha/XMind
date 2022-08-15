78ee8398d3b009db5019d53443cee2d51b5d1226
zhang ying <zhangyingmark@gmail>
2022-08-16 00:18:04 +0800

    vue:transition动画

---
+++ vue/learn-animation/src/01_动画的基本使用/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <button @click="toggle">显示/隐藏</button>
+        <transition name="my-animation">
+            <h2 v-if="isShow">Hello World</h2>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShow: true,
+        };
+    },
+    methods: {
+        toggle() {
+            this.isShow = !this.isShow;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.my-animation-enter-from,
+.my-animation-leave-to {
+    opacity: 0;
+}
+
+.my-animation-enter-to,
+.my-animation-leave-from {
+    opacity: 1;
+}
+
+.my-animation-enter-active,
+.my-animation-leave-active {
+    transition: opacity 2s ease;
+}
+</style>
