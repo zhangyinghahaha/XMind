26ef1fffa6529cd60f6246339cb8b4f213406a94
zhang ying <zhangyingmark@gmail>
2022-09-19 00:22:47 +0800

    vue:computed api

---
+++ vue/learn-composition-api/src/03_refapi/03_自定义ref.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>{{message}}</h2>
+        <input v-model="message" type="text" />
+    </div>
+</template>
+
+<script>
+    import {ref, shallowRef} from 'vue';
+    import debounceRef from './hook/useDebounceRef';
+    export default {
+        setup() {
+            const message = debounceRef('hello world');
+            return {message};
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
