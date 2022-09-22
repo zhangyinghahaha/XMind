30a3d44a8555af5ce647d55ec88cac3c4140ad63
zhang ying <zhangyingmark@gmail>
2022-09-22 23:47:40 +0800

    vue: composition api

---
+++ vue/learn-composition-api/src/03_refapi/08_watch侦听多个数据源.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h2 ref="title">{{info.age}}</h2>
+        <button @click="addAge">年龄+1</button>
+    </div>
+</template>
+
+<script>
+    import {ref, watch, watchEffect, reactive} from 'vue';
+    export default {
+        setup() {
+            const info = reactive({
+                name: 'why',
+                age: 18
+            });
+            const name = ref('zhang ying');
+
+            watch([info, name], (newValue, oldValue) => {
+              console.log('new: ', newValue);
+              console.log('old: ', oldValue);
+              console.log(newValue === oldValue);
+            });
+
+            const addAge = () => {
+                info.age++;
+            };
+
+            return {info,addAge};
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue/learn-composition-api/src/main.js
+++ vue/learn-composition-api/src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import App from './03_refapi/App.vue'
 
 const app = createApp(App);
-app.mixin({
-    created () {
-        console.log('全局Mixin');
-    },
-});
+// app.mixin({
+//     created () {
+//         console.log('全局Mixin');
+//     },
+// });
 app.mount('#app');
