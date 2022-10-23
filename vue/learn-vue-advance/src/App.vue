968fddf7e635eca3e6996957749c2357526a66bd
zhang ying <zhangyingmark@gmail>
2022-10-23 23:44:41 +0800

    vue

--- vue/learn-vue-advance/src/App.vue
+++ vue/learn-vue-advance/src/App.vue
@@ -1,49 +1,16 @@
 <template>
     <div>
-        <h2>自定义指令</h2>
-        <input type="text" v-focus>
-
-        <h2 v-test>当前计数: {{counter}}</h2>
-        <button @click="increment">+1</button>
-
-        <h2 v-format-time>1666192443848</h2>
+        <p>script setup语法糖</p>
+        <p>{{message}}</p>
     </div>
 </template>
 
-<script>
-import { ref, onMounted } from 'vue'; 
-export default {
-    directives: {
-        focus: {
-            mounted(element) {
-                console.log('focus mounted');
-                element.focus();
-            }
-        },
-        test: {
-            created() {
-                console.log('v-test created');
-            },
-            beforeMount() {
-                console.log('v-test beforeMount');
-            },
-            mounted() {
-                console.log('v-test mounted');
-            }
-        },
-    },
-    setup () {
-        const counter = ref(0);
-        const increment = () => counter.value++;
-
-        return {
-            counter,
-            increment
-        }
-    }
-}
+<script setup>
+im
+const message = 'hello';
 </script>
 
 <style lang="scss" scoped>
 
 </style>
+
