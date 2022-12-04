f30e96a0dde4d28a4616ffed8e2870de35776323
zhang ying <zhangyingmark@gmail>
2022-12-04 22:25:11 +0800

    feat(*): vue项目工程配置

---
+++ vue/learn-project/src/components/HelloEditorConfig.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { ref, reactive } from 'vue';
+  const a = 1;
+  let what = 'hhahah';
+  what = 'zhang ying';
+</script>
+
+<template>
+  <div> </div>
+</template>
+
+<style scoped></style>
